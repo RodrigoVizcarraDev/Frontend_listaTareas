3b124f6cd02f42e10f9c456a1031db27384a8684
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-07-12 03:10:24 -0300

    Codigo inservible quitado del componente FormularioTarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,10 +1,15 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, FormText } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
 import { useState, useEffect } from "react";
+import {useForm} from "react-hook-form";
+import Swal from "sweetalert2";
 
 const FormularioTarea = () => {
     //traemos las tareas del localStorage
     const tareaLocalStorage = JSON.parse(localStorage.getItem("listaTareas")) || [];
+
+    // react hook form
+    const {register, handleSubmit, formState:{errors},reset} = useForm();
 
     const [tarea, setTarea] = useState("");
     const [tareas, setTareas] = useState(tareaLocalStorage);
@@ -14,11 +19,16 @@ const FormularioTarea = () => {
         localStorage.setItem("listaTareas", JSON.stringify(tareas));
     }, [tareas]);
 
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setTareas([...tareas, { id: Date.now(), text: tarea }]);
-        setTarea("");
+
+    const onSubmit = (tarea) => {
+        console.log(tarea);
+        setTareas([...tareas, { id: Date.now(), text: tarea.nombreTarea }]);
+        reset();
+        Swal.fire(
+            "Listo",
+            "Tarea creada correctamente",
+            "success"
+        )
     };
 
     function borrarTarea(nombreTarea){
@@ -27,7 +37,7 @@ const FormularioTarea = () => {
     }
     return (
         <>
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group
                     className="mb-3 d-flex gap-1"
                     controlId="formBasicEmail"
@@ -35,13 +45,27 @@ const FormularioTarea = () => {
                     <Form.Control
                         type="text"
                         placeholder="Ingrese una tarea"
-                        onChange={(e) => setTarea(e.target.value)}
-                        value={tarea}
+                        // onChange={(e) => setTarea(e.target.value)}
+                        // value={tarea}
+                        {...register("nombreTarea",{
+                            required: "El nombre de la tarea es requerido",
+                            minLength: {
+                                value: 2,
+                                message: "El nombre de la tarea debe tener como minimo 2 caracteres"
+                            },
+                            maxLength: {
+                                value: 70,
+                                message: "El nombre de la tarea solo puede tener hasta 70 caracteres"
+                            }
+                        })}
                     />
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
                 </Form.Group>
+                <FormText className="text-danger">
+                        {errors.nombreTarea?.message}
+                </FormText>
             </Form>
             <ListaTareas listaTareas={tareas} borrarTarea={borrarTarea}></ListaTareas>
         </>
